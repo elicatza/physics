3937dc74005f7bd61c4c29a66b710b6145b6c9df
elicatza <eliza.cl@protonmail.com>
2023-09-03 08:33:38 +0200

    refactor: code is now readable!

--- main.c
+++ main.c
@@ -5,16 +5,20 @@
 #define VEC2D_IMPLEMENTATION
 #include "./vec2d.h"
 
-#define WIDTH 700
-#define HEIGHT 700
+#define WIDTH 750
+#define HEIGHT 750
 #define FPS 60
 #define GRAVITY 9.81f
 
-#define M_BG    GetColor(0x282828ff)
-#define M_BLUE  GetColor(0x55cdfcff)
-#define M_PINK  GetColor(0xf7a8b8ff)
-#define M_RED   GetColor(0xcc241dff)
-#define M_GREEN GetColor(0x98971aff)
+#define M_BG     GetColor(0x282828ff)
+#define M_FG     GetColor(0xebdbb2ff)
+#define M_BLUE   GetColor(0x458588ff)
+#define M_AQUA   GetColor(0x689d6aff)
+#define M_PURPLE GetColor(0xb16286ff)
+#define M_RED    GetColor(0xcc241dff)
+#define M_GREEN  GetColor(0x98971aff)
+#define M_YELLOW GetColor(0xd79921ff)
+#define M_ORANGE GetColor(0xd65d0eff)
 
 // Kinematics
 typedef struct {
@@ -23,22 +27,27 @@ typedef struct {
     Vec2d acc;
     float mass;
     float friction;
-    // bool is_floating;
     float angle;
 } Kin;
 
 
-void kin_draw(Kin kin, uint32_t size, float thick)
+void kin_draw_square(Kin kin, uint32_t size, float thick, Color color)
 {
-    Vec2d base = vec2d_sub(kin.pos, vec2d_rotate((Vec2d) { size / 2, 0 }, -kin.angle));
-    // Vec2d base = vec2d_sub(kin.pos, vec2d_scale(kin.pos, size / 2)));
-    Vec2d left = vec2d_rotate((Vec2d) { size, 0 }, - kin.angle);
-    Vec2d up = vec2d_rotate((Vec2d) { size, 0 }, - kin.angle - PI / 2);
-    Vec2d upleft = vec2d_add(left, up);
-    DrawLineEx(base, vec2d_add(base, left), thick, M_BLUE);
-    DrawLineEx(base, vec2d_add(base, up), thick, M_BLUE);
-    DrawLineEx(vec2d_add(base, left), vec2d_add(base, upleft), thick, M_BLUE);
-    DrawLineEx(vec2d_add(base, up), vec2d_add(base, upleft), thick, M_BLUE);
+    // Find (r)elative (l)eft and (u)p vector
+    Vec2d rel_r = vec2d_rotate((Vec2d) { size, 0 }, -kin.angle);
+    Vec2d rel_u = vec2d_rotate((Vec2d) { size, 0 }, -kin.angle - PI / 2);
+
+    // Offset (d)own (l)eft corner to put center of mass in the center.
+    Vec2d dl = vec2d_sub(kin.pos, vec2d_scale(vec2d_add(rel_r, rel_u), 0.5));
+
+    Vec2d dr = vec2d_add(dl, rel_r);
+    Vec2d ul = vec2d_add(dl, rel_u);
+    Vec2d ur = vec2d_add(dl, vec2d_add(rel_r, rel_u));
+
+    DrawLineEx(dl, dr, thick, color);
+    DrawLineEx(dl, ul, thick, color);
+    DrawLineEx(dr, ur, thick, color);
+    DrawLineEx(ul, ur, thick, color);
 }
 
 Vec2d kin_force_normal(Kin kin)
@@ -66,16 +75,17 @@ Vec2d kin_force_friction(Kin kin)
 
 void draw_arrow(Vec2d base, Vec2d rel, Color color, char *text)
 {
-    Vec2d end = vec2d_add(base, rel);
-    Vec2d linedir = vec2d_scale(vec2d_unitinterval(rel), 16);
-    Vec2d arrow_length = vec2d_scale(linedir, 1.2);
-    Vec2d arrow_base = vec2d_sub(end, arrow_length);
-    Vec2d k3 = vec2d_add(arrow_base, vec2d_scale(linedir, 1.2));
-    Vec2d k1 = vec2d_add(arrow_base, vec2d_rotate(linedir, PI / 2));
-    Vec2d k2 = vec2d_add(arrow_base, vec2d_rotate(linedir, -PI / 2));
-    DrawTriangle(k2, k1, k3, color);
+    Vec2d tipp = vec2d_add(base, rel);
+    Vec2d arrow_rel_tipp = vec2d_scale(vec2d_unitinterval(rel), 16);
+    Vec2d arrow_base = vec2d_sub(tipp, arrow_rel_tipp);
+
+    // Vec2d k3 = vec2d_add(arrow_base, arrow_rel_tipp);
+    Vec2d k1 = vec2d_add(arrow_base, vec2d_scale(vec2d_rotate(arrow_rel_tipp, PI / 2), 0.66));
+    Vec2d k2 = vec2d_add(arrow_base, vec2d_scale(vec2d_rotate(arrow_rel_tipp, -PI / 2), 0.66));
+
+    DrawTriangle(k2, k1, tipp, color);
     DrawLineEx(base, arrow_base, 6, color);
-    DrawText(text, end.x + 20, end.y + 20, 30, color);
+    DrawText(text, tipp.x + 20, tipp.y + 20, 30, color);
 }
 
 int main(void)
@@ -83,7 +93,7 @@ int main(void)
     InitWindow(WIDTH, HEIGHT, "Physics");
     SetTargetFPS(FPS);
     Kin kin = {
-        .pos = (Vec2d) { 500, 500 },
+        .pos = (Vec2d) { WIDTH / 2, HEIGHT / 2 },
         .vel = (Vec2d) { 100, 100 },
         .acc = (Vec2d) { 0, GRAVITY },
         .friction = 0.17f,
@@ -109,12 +119,12 @@ int main(void)
                     .y = kin.pos.y,
                     .width = 10,
                     .height = 20,
-                    }, kin.pos, kin.angle, M_PINK);
+                    }, kin.pos, kin.angle, M_PURPLE);
 
             draw_arrow(kin.pos, kin_force_gravity(kin), M_GREEN, "G");
             draw_arrow(kin.pos, kin_force_normal(kin), M_BLUE, "N");
-            draw_arrow(kin.pos, kin_force_friction(kin), M_PINK, "R");
-            kin_draw(kin, 100, 5);
+            draw_arrow(kin.pos, kin_force_friction(kin), M_PURPLE, "R");
+            kin_draw_square(kin, 100, 5, M_FG);
 
             char angle[32];
             snprintf(angle, 32, "%.2f", kin.angle * 180 / PI);
